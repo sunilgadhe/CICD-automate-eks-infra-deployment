7ac0d112e4f6bdb98432c67159357e1afc704ac4
SUNIL GADHE <89225052+sunilgadhe@users.noreply.github.com>
2025-07-02 11:03:48 +0100

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,23 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name                                           = "${var.cluster_name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name                                           = "${var.cluster_name}-private-${count.index + 1}"
+    "kubernetes.io/cluster/${var.cluster_name}"    = "shared"
+    "kubernetes.io/role/internal-elb"              = "1"
+  }
+}
